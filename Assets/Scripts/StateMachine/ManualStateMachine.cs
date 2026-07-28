e700204f3283cfcb7e071b044495728ea4ecd7f2
Mono-Ling <zouzongfeng070812@163.com>
2026-07-29 02:13:26 +0800

    完善手动启动状态机的启动逻辑

--- Assets/Scripts/StateMachine/ManualStateMachine.cs
+++ Assets/Scripts/StateMachine/ManualStateMachine.cs
@@ -5,12 +5,21 @@ using UnityEngine;
 public class ManualStateMachine : StateMachine
 {
     private bool _isStart => blackboard != null;
-    protected override void Start() { }
+    protected override void Start()
+    {
+        if (initState == null)
+            Debug.LogWarning("手动启动状态机】初始状态为空");
+    }
     public void InitStateMachine(Blackboard blackboard)
     {
         if (blackboard == null)
+        {
             Debug.LogError("【手动启动状态机】黑板为空，启动失败");
+            return;
+        }
         this.blackboard = blackboard;
+        currState = GameObject.Instantiate(initState);
+        currState.OnEnter(this.blackboard);
     }
     protected override void Update()
     {
