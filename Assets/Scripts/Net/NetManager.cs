b3c895bd1de345ed7ea72c4f2aa0157334a8564c
Mono-Ling <zouzongfeng070812@163.com>
2026-08-04 17:51:20 +0800

    拆分客户端程序集、优化编译速度

--- Assets/Scripts/Net/NetManager.cs
+++ Assets/Scripts/Net/NetManager.cs
@@ -5,7 +5,6 @@ using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
 using Message;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class NetManager : SingleMono<NetManager>
