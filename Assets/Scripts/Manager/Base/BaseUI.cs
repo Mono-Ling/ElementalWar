7f23b26a906f8adf01a43152644c4bfa3d5e8be1
Mono-Ling <zouzongfeng070812@163.com>
2026-08-11 19:42:50 +0800

    新增UI系统

---
+++ Assets/Scripts/Manager/Base/BaseUI.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CanvasGroup))]
+public abstract class BaseUI : MonoBehaviour
+{
+    public float showTime = 1f;
+    public float hideTime = 2f;
+    protected CanvasGroup canvasGroup;
+    protected float startAnimationTime;
+    protected Action<BaseUI> callback;
+    protected bool isShow;
+    protected virtual void Awake()
+    {
+        canvasGroup = GetComponent<CanvasGroup>();
+        if (canvasGroup == null)
+            Debug.Log("【UI】CanvasGroup获取失败");
+    }
+    protected virtual void Update()
+    {
+        if (isShow)
+            ShowAnimation();
+        else
+            HideAnimation();
+    }
+    public virtual void Show(Action<BaseUI> action = null, bool isAnimation = true)
+    {
+        if (!isAnimation)
+        {
+            action?.Invoke(this);
+            return;
+        }
+        canvasGroup.alpha = 0f;
+        startAnimationTime = Time.time;
+        callback = action;
+
+        isShow = true;
+    }
+    public virtual void Hide(Action<BaseUI> action = null, bool isAnimation = true)
+    {
+        if (!isAnimation)
+        {
+            action?.Invoke(this);
+            return;
+        }
+        canvasGroup.alpha = 1f;
+        startAnimationTime = Time.time;
+        callback = action;
+
+        isShow = false;
+    }
+    protected virtual void ShowAnimation()
+    {
+        if (Time.time - startAnimationTime > showTime)
+        {
+            callback?.Invoke(this);
+            callback = null;
+            canvasGroup.alpha = 1;
+            return;
+        }
+        float t = (Time.time - startAnimationTime) / showTime;
+        t = Mathf.Clamp01(t);
+        canvasGroup.alpha = t;
+    }
+    protected virtual void HideAnimation()
+    {
+        if (Time.time - startAnimationTime > hideTime)
+        {
+            callback?.Invoke(this);
+            callback = null;
+            canvasGroup.alpha = 0;
+            return;
+        }
+        float t = (Time.time - startAnimationTime) / hideTime;
+        t = Mathf.Clamp01(t);
+        canvasGroup.alpha = 1 - t;
+    }
+}
---
+++ Assets/Scripts/Manager/Framework/UIManager.cs
@@ -0,0 +1,199 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : SingleMono<UIManager>
+{
+    public const string FOLDER_PATH = "UI";
+    public const string CANVAS_PATH = "Canvas";
+    public const string EVENT_SYSTEM_PATH = "EventSystem";
+    private Transform _dynamicCanvas;
+    private Transform _staticCanvas;
+    private Stack<BaseUI> _panelStack = new();
+    private BaseUI CurrentPanel
+    {
+        get
+        {
+            if (_panelStack.Count == 0)
+                return null;
+            return _panelStack.Peek();
+        }
+    }
+    void Awake()
+    {
+        var canvasPrefab = Resources.Load<GameObject>($"{FOLDER_PATH}/{CANVAS_PATH}");
+        if (canvasPrefab == null)
+        {
+            Debug.LogError("【UI管理器】Canvas加载失败");
+            return;
+        }
+        var canvasObj = Instantiate(canvasPrefab, transform, false);
+        if (canvasObj == null)
+        {
+            Debug.LogError("【UI管理器】Canvas创建失败");
+            return;
+        }
+        _dynamicCanvas = canvasObj.transform;
+        canvasObj = Instantiate(canvasPrefab, transform, false);
+        _staticCanvas = canvasObj.transform;
+
+        var eventPrefab = Resources.Load<GameObject>($"{FOLDER_PATH}/{EVENT_SYSTEM_PATH}");
+        if (eventPrefab == null)
+        {
+            Debug.LogError("【UI管理器】EventSystem加载失败");
+            return;
+        }
+        var eventObj = Instantiate(eventPrefab, transform, false);
+        if (eventObj == null)
+            Debug.LogError("【UI管理器】EventSystem创建失败");
+    }
+    public bool TryGetCurrentPanel<T>(out T panel) where T : BaseUI
+    {
+        if (CurrentPanel is T findPanel)
+        {
+            panel = findPanel;
+            return true;
+        }
+        Debug.LogWarning($"【UI管理器】面板{typeof(T)}不存在");
+        panel = null;
+        return false;
+    }
+    public T ShowPanel<T>(Action<BaseUI> action = null, bool isAnimation = true) where T : BaseUI
+    {
+        var type = typeof(T);
+        if (CurrentPanel is T findePanel)
+        {
+            Debug.LogWarning($"【UI管理器】{type}已存在");
+            return findePanel;
+        }
+        var path = type.Name;
+        var panelPrefab = Resources.Load<GameObject>($"{FOLDER_PATH}/{path}");
+        if (panelPrefab == null)
+        {
+            Debug.LogError($"【UI管理器】面板{path}加载失败");
+            return null;
+        }
+
+        var panelObj = Instantiate(panelPrefab, _staticCanvas, true);
+        if (panelObj == null)
+        {
+            Debug.LogError($"【UI管理器】面板{typeof(T)}创建失败");
+            return null;
+        }
+
+        if (!InitPanel<T>(panelObj, out var panel))
+        {
+            Debug.LogError($"【UI管理器】面板{path}初始化失败");
+            return null;
+        }
+
+        CurrentPanel?.Hide(DisablePanel, isAnimation);
+
+        panel.Show(action, isAnimation);
+        _panelStack.Push(panel);
+        return panel;
+    }
+    public IEnumerator ShowPanelAsync<T>(Action<T> asyncCallback = null, Action<BaseUI> showCallback = null, bool isAnimation = true) where T : BaseUI
+    {
+        var type = typeof(T);
+        if (CurrentPanel is T findePanel)
+        {
+            Debug.LogWarning($"【UI管理器】{type}已存在");
+            yield break;
+        }
+        var path = type.Name;
+        var req = Resources.LoadAsync<GameObject>($"{FOLDER_PATH}/{path}");
+        yield return req;
+
+        if (req.asset is not GameObject panelPrefab)
+        {
+            Debug.LogError($"【UI管理器】面板{path}加载失败");
+            asyncCallback?.Invoke(null);
+            yield break;
+        }
+
+        var instantiateReq = InstantiateAsync(panelPrefab);
+        yield return instantiateReq;
+
+        if (instantiateReq.Result[0] is not GameObject panelObj)
+        {
+            Debug.LogError($"【UI管理器】面板{typeof(T)}创建失败");
+            asyncCallback?.Invoke(null);
+            yield break;
+        }
+        if (!InitPanel<T>(panelObj, out var panel))
+        {
+            Debug.LogError($"【UI管理器】面板{path}初始化失败");
+            asyncCallback?.Invoke(null);
+            yield break;
+        }
+        CurrentPanel?.Hide(DisablePanel, isAnimation);
+
+        panelObj.transform.SetParent(_staticCanvas, true);
+        panel.Show(showCallback, isAnimation);
+        _panelStack.Push(panel);
+        asyncCallback?.Invoke(panel);
+    }
+    public void HidePanel(Action<BaseUI> action = null, bool isAnimation = true)
+    {
+        if (CurrentPanel == null)
+        {
+            Debug.LogWarning("【UI管理器】当前没有打开面板");
+            return;
+        }
+        action += DestroyPanel;
+        CurrentPanel.Hide(action, isAnimation);
+        _panelStack.Pop();
+        CurrentPanel?.Show(EnablePanel, isAnimation);
+    }
+    public T BufferShowUI<T>(Action<BaseUI> action = null, bool isAnimation = true) where T : BaseUI
+    {
+        var path = typeof(T).Name;
+        var obj = MonoObjectPool.Instance.GetObject($"{FOLDER_PATH}/{path}");
+        if (obj == null)
+        {
+            Debug.LogError($"【UI管理器】UI{path}创建失败");
+            return null;
+        }
+        var ui = obj.GetComponent<T>();
+        if (ui == null)
+        {
+            Debug.LogError($"【UI管理器】UI{path}获取失败");
+            MonoObjectPool.Instance.PutObject(obj);
+            return null;
+        }
+        obj.transform.SetParent(_dynamicCanvas, true);
+        ui.Show(action, isAnimation);
+        return ui;
+    }
+    public void BufferHideUI(BaseUI uI, Action<BaseUI> action = null, bool isAnimation = true)
+    {
+        if (uI == null)
+            return;
+        action += BufferDestroyUI;
+        uI.Hide(action, isAnimation);
+    }
+    private bool InitPanel<T>(GameObject panelObj, out T panel) where T : BaseUI
+    {
+        panel = null;
+        if (panelObj == null)
+            return false;
+        panel = panelObj.GetComponent<T>();
+        if (panel == null)
+        {
+            Debug.LogError($"【UI管理器】面板{typeof(T)}获取失败失败");
+            Destroy(panelObj);
+            return false;
+        }
+        return true;
+    }
+    private void DisablePanel(BaseUI uI)
+    => uI.gameObject.SetActive(false);
+    private void EnablePanel(BaseUI uI)
+    => uI.gameObject.SetActive(true);
+    private void DestroyPanel(BaseUI uI)
+    => Destroy(uI.gameObject);
+    private void BufferDestroyUI(BaseUI uI)
+    => MonoObjectPool.Instance.PutObject(uI.gameObject);
+}
